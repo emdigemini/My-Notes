b77873a27ec5f998c0578d34145517020ebe92c1
Emdi <markdhenn05@gmail.com>
2025-08-22 08:07:45 +0800

    Update list.js

--- list.js
+++ list.js
@@ -4,26 +4,60 @@ const params = new URLSearchParams(window.location.search);
 const listId = params.get('id');
 const list = todoList.find(item => item.id === listId);
 const app = document.querySelector('.app');
+const toggle = document.querySelector('.toggle');
+const checkChar = document.querySelector('.checkBox');
+
 const renderNotes = () => {
   app.innerHTML = 
   `
     <header><a class="back" href="index.html">&#10148;</a><h1>${list.title}</h1></header>
-    <div class="notepad" contenteditable="true" spellcheck="true" aria-label="Notepad">${list.text}
+    <div class="notepad" contenteditable="true" spellcheck="true" aria-label="Notepad">
+    ${list.text}
     </div>
   `;
 
   const notepad = document.querySelector('.notepad');
-
+  notepad.addEventListener('keydown', (e) => {
+    if(e.key === 'Tab'){
+      e.preventDefault()
+      document.execCommand('insertText', false, '\t');
+    }
+  })
   notepad.addEventListener('input', () => {
     list.text = notepad.innerText;
     localStorage.setItem('todoList', JSON.stringify(todoList));
+    countCharAndWords();
   })
+}
 
+function countCharAndWords(){
+  console.log(list.text);
+  const charCount = list.text.length;
+  const wordCount = list.text.split(/\s+/).filter(word => word.length > 0).length;
+  console.log(charCount);
 }
 
 if(!list){
-  console.log('list not found!');;
+  console.log('list not found!');
 }else {
   renderNotes();
 }
+
+/** Toggle Dark Mode */
+let darkMode = false;
+toggle.addEventListener('click', () => {
+  if(!darkMode){
+    document.body.classList.add('dark-mode');
+    app.classList.remove('slide-inLightMode');
+    app.classList.add('slide-inDarkMode');
+    darkMode = true;
+    toggle.innerHTML = `&#127774; Light Mode`
+  }else {
+    document.body.classList.remove('dark-mode');
+    app.classList.add('slide-inLightMode');
+    app.classList.remove('slide-inDarkMode');    
+    darkMode = false;
+    toggle.innerHTML = `&#127769; Dark Mode`
+  }
+})
 
